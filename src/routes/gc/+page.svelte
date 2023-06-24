f6b1b7a61908922f647f74dd15ee0793b15b81a2
lucaspontoexe <ltx16@hotmail.com>
2023-06-24 00:37:37 -0300

    o texto tá tomando forma - segura o commit

---
+++ src/routes/gc/+page.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+	// import type { PageData } from './$types';
+
+	// export let data: PageData;
+</script>
+
+<main>
+	<div class="lowerthird">
+		<div class="title-top">topo do rolê</div>
+		<div class="line" />
+		<div class="title-bottom">
+			parte de baixo
+			com linha dupla
+		</div>
+	</div>
+</main>
+
+<style lang="scss">
+    /* body já tá sem margin/padding */
+	$font-shadow-filter: drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.2));
+
+	main {
+        /* cor pro debug, apagar depois */
+		background-color: black;
+		width: 100vw;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+	}
+
+	.lowerthird {
+        /* a gente usa a linha dos 90% como margem */
+		margin-left: 5vw;
+		margin-right: 5vw;
+		margin-bottom: 5vh;
+
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: end;
+
+		text-align: center;
+
+        /* pra fins de debugging - apagar depois */
+        background-color: rgba(0, 188, 255, 0.3);
+	}
+
+	.title-top {
+		font-family: "Lora Italic";
+		font-size: 48px;
+		filter: $font-shadow-filter;
+		color: white;
+	}
+
+    .line {
+        height: 2px;
+        /* a gente tem que ver como vai calcular/animar isso. 80% do título? quantidade de caracteres? Talvez JS neles. */
+        width: 60%; 
+
+        background-color: white;
+    }
+
+	.title-bottom {
+		font-family: "Poppins Bold";
+		font-size: 72px; // em, etc.
+		white-space: pre-line;
+		line-height: 1.2;
+		color: $main-title-color;
+		filter: $font-shadow-filter;
+	}
+</style>
