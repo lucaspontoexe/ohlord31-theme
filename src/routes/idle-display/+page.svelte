06527ccd19f0bdefc8eaf61710e59f71f01dc81f
lucaspontoexe <ltx16@hotmail.com>
2023-08-16 00:11:53 -0300

    tá criado um arquivo pra recomeçar a "idle-screen"

---
+++ src/routes/idle-display/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { websocketStore, fallback } from '$lib/websocketStore';
+
+	export let data: PageData;
+    const items = websocketStore('idle-screen-slides', fallback['idle-screen-slides']);
+	
+</script>
+
+<main />
+
+<pre class="debug">{JSON.stringify(data)}</pre>
+
+<br />
+
+<pre class="debug">{JSON.stringify($items, null, 4)}</pre>
+
