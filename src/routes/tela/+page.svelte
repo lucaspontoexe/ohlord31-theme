21202eb8c1b5beb3ae9647043eef082335f1a377
lucaspontoexe <ltx16@hotmail.com>
2023-08-24 09:39:45 -0300

    esquleto da tela liturgia

--- src/routes/tela/+page.svelte
+++ src/routes/tela/+page.svelte
@@ -6,7 +6,21 @@
 	const items = websocketStore('idle-screen-slides', fallback['idle-screen-slides']);
 </script>
 
-<main />
+<main>
+
+	<section class="tela-liturgia">
+		<div class="title">
+			1º Domingo do Tempo Legal
+		</div>
+
+		<div class="liturgia">
+			<div class="nome">1ª Leitura</div>
+			<div class="referencia">Lc 12,3-4</div>
+			<div class="descricao">é salmo agora</div>
+		</div>
+	</section>
+
+</main>
 
 <pre class="debug">{JSON.stringify(data)}</pre>
 <pre class="debug">{JSON.stringify($items, null, 4)}</pre>
