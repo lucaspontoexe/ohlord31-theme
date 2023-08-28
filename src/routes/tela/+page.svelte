0534cf6793264c61037b063f021b348deb759f4d
lucaspontoexe <ltx16@hotmail.com>
2023-08-28 17:49:21 -0300

    alinhamento, fontes, formatação e experimentando <hr>

--- src/routes/tela/+page.svelte
+++ src/routes/tela/+page.svelte
@@ -9,24 +9,21 @@
 </script>
 
 <main>
-
 	<section class="tela-liturgia">
 		<div class="title">
 			{$liturgia.nome}
 		</div>
 
 		<div class="liturgia">
-			{#each $liturgia.leituras as leitura}	
+			{#each $liturgia.leituras as leitura}
 				<div class="leitura">
 					<div class="nome">{leitura.nome}</div>
 					<div class="referencia">{leitura.referencia}</div>
-					{#if leitura.descricao} <div class="descricao">{leitura.descricao}</div> {/if}
+					{#if leitura.descricao} <div class="descricao">{leitura.descricao} </div> {/if}
 				</div>
 			{/each}
-
 		</div>
 	</section>
-
 </main>
 
 <pre class="debug">{JSON.stringify(data)} {'\n\n'} {JSON.stringify($items, null, 4)}</pre>
@@ -34,15 +31,15 @@
 <style lang="scss">
 	.tela-liturgia {
 		display: flex;
-		align-items: center;		
+		align-items: center;
 		justify-content: space-between;
 		padding: 0px 4vw;
 		gap: 20px;
 		height: 100vh;
-		
+
 		.title {
 			max-width: 50%;
-			font-family: "Poppins Bold";
+			font-family: 'Poppins Bold';
 			font-size: 8em;
 		}
 
@@ -52,21 +49,28 @@
 
 			.leitura {
 				margin: 2em 0px;
+
+				// hr {
+				// 	width: 80%;
+				// 	border-style: solid;
+				// 	margin: 0px auto;
+				// }
 			}
 
 			.nome {
-				font-family: "Lora";
+				font-family: 'Lora';
 				font-size: 3em;
 			}
 
 			.referencia {
 				// apply poppins
-				font-family: "Poppins Bold";
+				font-family: 'Poppins Bold';
 				font-size: 5em;
+				margin: 0px;
 			}
 
 			.descricao {
-				font-family: "Lora Italic";
+				font-family: 'Lora Italic';
 				font-size: 3em;
 			}
 		}
