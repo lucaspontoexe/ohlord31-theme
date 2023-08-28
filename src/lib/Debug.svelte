bb9eb6b7c24ad981830e9117914e82ec5aedfd5a
lucaspontoexe <ltx16@hotmail.com>
2023-08-28 17:57:40 -0300

    add "debug" component

---
+++ src/lib/Debug.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let data: any;
+</script>
+
+<pre class="debug">{JSON.stringify(data, null, 4)}</pre>
+
+<style lang="scss">
+	pre.debug {
+		position: fixed;
+		background-color: rgba(black, 0.25);
+		color: white;
+		top: 20px;
+		left: 20px;
+	}
+</style>
