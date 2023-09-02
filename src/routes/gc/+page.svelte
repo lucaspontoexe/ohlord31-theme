5366d0dab8c6fb52dfd64e2374c51a723d9f52e8
lucaspontoexe <ltx16@hotmail.com>
2023-09-02 14:18:20 -0300

    gc ofertório tá componentizado

--- src/routes/gc/+page.svelte
+++ src/routes/gc/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
 	import Standard from './Standard.svelte';
-	import qr_pix from '$lib/qr_pix.png';
+	import Oferta from './Oferta.svelte';
 
 	import { websocketStore, fallback } from '$lib/websocketStore';
-	import { fade, fly } from 'svelte/transition';
-
 	const lt_display = websocketStore('lower-third-display', fallback['lower-third-display']);
 </script>
 
@@ -16,17 +14,7 @@
 	{/if}
 
 	{#if $lt_display.layout === 'ofertório'}
-		<div class="pix" transition:fade={{duration: 1000}}>
-			<!-- por que não só colocar a imagem png aqui? -->
-			<div class="qrcode-container">
-				<img src="{qr_pix}" alt="código qr" />
-			</div>
-			<div class="text">
-				<div class="headline" in:fly={{x: -20, delay: 200, duration: 600}}>Faça sua oferta!</div>
-				<div class="line" in:fly={{x: -20, delay: 400, duration: 600}}>linha 2</div>
-				<div class="line" in:fly={{x: -20, delay: 600, duration: 600}}>linha 3</div>
-			</div>
-		</div>
+		<Oferta />
 	{/if}
 
 	{#if $lt_display.layout === 'comunhão'}
@@ -46,34 +34,6 @@
 		display: flex;
 		flex-direction: column;
 		justify-content: flex-end;
-	}
-
-	.pix {
-		display: flex;
-		flex-direction: row;
-		
-		padding-left: 5vw;
-		padding-bottom: 5vh;
-		padding-top: 7em;
-		gap: 2em;
-		
-		background: linear-gradient(4deg, var(--lowerthird-bg-color) -5%, rgba(0, 0, 0, 0) 60%);
-		filter: $font-shadow-filter;
-
-		.headline {
-			@include headline;
-			font-size: 3em;
-		}
-
-		img {
-			border-radius: 5px;
-		}
-
-		.line {
-			color: white;
-			font-family: 'Lora';
-			font-size: 2em;
-		}
 	}
 
 	.debug {
---
+++ src/routes/gc/Oferta.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import qr_pix from '$lib/qr_pix.png';
+	import { fade, fly } from 'svelte/transition';
+</script>
+
+<div class="pix" transition:fade={{ duration: 1000 }}>
+	<div class="qrcode-container">
+		<img src={qr_pix} alt="código qr" />
+	</div>
+	<div class="text">
+		<div class="headline" in:fly={{ x: -20, delay: 200, duration: 600 }}>Faça sua oferta!</div>
+		<div class="line" in:fly={{ x: -20, delay: 400, duration: 600 }}>linha 2</div>
+		<div class="line" in:fly={{ x: -20, delay: 600, duration: 600 }}>linha 3</div>
+	</div>
+</div>
+
+<style lang="scss">
+	/* body já tá sem margin/padding */
+	$font-shadow-filter: drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.2));
+
+	.pix {
+		display: flex;
+		flex-direction: row;
+
+		padding-left: 5vw;
+		padding-bottom: 5vh;
+		padding-top: 7em;
+		gap: 2em;
+
+		background: linear-gradient(4deg, var(--lowerthird-bg-color) -5%, rgba(0, 0, 0, 0) 60%);
+		filter: $font-shadow-filter;
+
+		.headline {
+			@include headline;
+			font-size: 3em;
+		}
+
+		img {
+			border-radius: 5px;
+		}
+
+		.line {
+			color: white;
+			font-family: 'Lora';
+			font-size: 2em;
+		}
+	}
+</style>
