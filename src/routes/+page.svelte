6ae6c72aa7173beb48ce3444557563702ddea421
lucaspontoexe <ltx16@hotmail.com>
2023-10-14 21:21:45 -0300

    agora o app é construído pra página ser servida pelo caminho /v31 (precisei modificar o server pra isso, mas tá funcionando)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
+<script>
+    import { base } from "$app/paths";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<a href="/gc">olha o gc, olha</a>
-<a href="/tela">tela observe</a>
+<a href="{base}/gc">olha o gc, olha</a>
+<a href="{base}/tela">tela observe</a>
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -19,7 +20,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({fallback: 'index.html'})
+		adapter: adapter({fallback: 'index.html'}),
+		paths: {
+			base: '/v31',
+		}
 	}
 };
 
