7403d28cf8952f23a746d5f5c42a6952c0826e42
lucaspontoexe <ltx16@hotmail.com>
2024-05-08 16:14:33 -0300

    add thumbnail code

--- src/routes/editor/thumbnail/+page.svelte
+++ src/routes/editor/thumbnail/+page.svelte
@@ -1,17 +1,32 @@
 <script lang="ts">
 	import { websocketStore, fallback } from '$lib/websocketStore';
+	import { onMount } from 'svelte';
+	import { drawCanvas } from './drawThumbnail';
 
 	const liturgia = websocketStore('liturgia', fallback['liturgia']);
+	// podia usar action? podia
+	let canvas: HTMLCanvasElement;
+	const theTitle = { top: 'aaaa', main: 'bbb', bottom: 'cccc' };
+	onMount(() => {
+		drawCanvas(canvas, theTitle, { background: new Image() }, true);
+	});
 </script>
 
-
 <main>
-    <h1>thumbnail</h1>
-    <canvas>
-        
-    </canvas>
+	<h1>thumbnail</h1>
+	<canvas width="1920" height="1080" bind:this={canvas} />
 
-    <div class="details">
-        {$liturgia.nome} | {new Date().toDateString()}
-    </div>
+	<div class="details">
+		{$liturgia.nome} | {new Date().toDateString()}
+	</div>
+
+	<input type="text" name="" id="a" bind:value={$liturgia.nome} />
 </main>
+
+<style>
+	canvas {
+		width: 960px;
+        border: 1px solid gray;
+        background-color: green;
+	}
+</style>
---
+++ src/routes/editor/thumbnail/drawThumbnail.ts
@@ -0,0 +1,53 @@
+type obj_title = {top: string, main: string, bottom: string}
+type img_object = {background: HTMLImageElement}
+
+export function drawCanvas(canvas: HTMLCanvasElement, title: obj_title, imageObject: img_object, drawDebugLines = false) {
+    if (!canvas) return;
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const lines = title.main
+        .split('\n')
+        .map((k) => k.trim()),
+      i = 100,
+      p = canvas.width - 200,
+      w = 96,
+      z = 40,
+      titleMargin = 40,
+      titleLineHeight = 144,
+      h1_lineHeight = titleLineHeight + titleMargin,
+      titleHeight = h1_lineHeight * lines.length,
+      totalHeight = titleHeight + (w * 2 + z);
+    let availableHeight = canvas.height - (lines.length > 2 ? 136 : 0);
+    title.top || (availableHeight -= w);
+    const v = (availableHeight - totalHeight) / 2,
+      m = v + w + z,
+      u = m + titleHeight;
+    ctx.save(),
+      imageObject.background && ctx.drawImage(imageObject.background, 0, 0),
+      (ctx.font = "italic 96px Lora"),
+      (ctx.textBaseline = "top"),
+      (ctx.textAlign = "center"),
+      (ctx.fillStyle = "white"),
+      ctx.fillText(title.top, 960, v),
+      ctx.fillText(title.bottom, 960, u),
+      (ctx.font = "144px Poppins"),
+      (ctx.fillStyle = "#F6D084"),
+      (ctx.shadowBlur = 12),
+      (ctx.shadowOffsetY = 12),
+      (ctx.shadowColor = "rgba(0, 0, 0, 0.25)"),
+      lines.forEach((line, index) => {
+        ctx.fillText(line, 960, m + h1_lineHeight * index);
+      }),
+      ctx.restore(),
+      drawDebugLines &&
+        (lines.forEach((_line, index) => {
+          ctx.strokeRect(i, m + h1_lineHeight * index, p, titleLineHeight);
+        }),
+        ctx.strokeRect(i, v, p, w),
+        ctx.strokeRect(i, u, p, w),
+        ctx.strokeRect(0, availableHeight, 1920, 1080),
+        (ctx.strokeStyle = "red"),
+        ctx.strokeRect(i, v, p, totalHeight),
+        console.log("total height:", totalHeight),
+        console.log("available height:", availableHeight)); // tem descrição disso no PC da igreja
+  }
