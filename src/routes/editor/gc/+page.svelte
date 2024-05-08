bb65fd8289326a1993216d5998ceaa657945640a
lucaspontoexe <ltx16@hotmail.com>
2024-05-08 11:53:01 -0300

    editor gc: agora mostra os itens

--- src/routes/editor/gc/+page.svelte
+++ src/routes/editor/gc/+page.svelte
@@ -1,10 +1,32 @@
-<!-- <script lang="ts">
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
-</script> -->
+<script>
+	import { fallback } from '$lib/fallback';
+	import { websocketStore } from '$lib/websocketStore2';
+
+	const lower_thirds = websocketStore('lower-third-items', fallback['lower-third-items']);
+</script>
 
 <main>
-    <h1>editor gc</h1>
-    <p>copiar tema do OBS etc</p>
+	<h1>editor gc</h1>
+	<p>copiar tema do OBS etc</p>
+
+	<div class="blocks">
+		{#each $lower_thirds as theThing}
+			<div class="thething">
+				{#if theThing.layout === 'default'}
+					<input type="text" bind:value={theThing.props.top} />
+					<textarea bind:value={theThing.props.bottom} />
+				{:else}
+					<p>{theThing.layout}</p>
+				{/if}
+                <button>Mostar na tela</button>
+			</div>
+		{/each}
+	</div>
 </main>
+
+<style>
+	.thething {
+		display: flex;
+		flex-direction: column;
+	}
+</style>
