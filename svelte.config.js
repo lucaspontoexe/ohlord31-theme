073cab8e057514b06313113f3f842950a01c3307
lucaspontoexe <ltx16@hotmail.com>
2024-05-08 10:57:43 -0300

    svelte config: remove unused line

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
