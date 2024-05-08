d2eaf7cf9ac16a5b95db57bf3f8594f8d83e257d
lucaspontoexe <ltx16@hotmail.com>
2024-05-08 12:26:50 -0300

    tem uso de stores

--- src/routes/editor/liturgia/+page.svelte
+++ src/routes/editor/liturgia/+page.svelte
@@ -1,10 +1,27 @@
-<!-- <script lang="ts">
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
-</script> -->
+<script lang="ts">
+	import { websocketStore, fallback } from '$lib/websocketStore';
+
+	const liturgia = websocketStore('liturgia', fallback['liturgia']);
+</script>
+
 
 <main>
     <h1>liturgia</h1>
     <p>amém</p>
+
+    <header>
+        <p>{$liturgia.nome}</p>
+        <p>cor: {$liturgia.cor}</p>
+    </header>
+
+    <h2>leituras</h2>
+    <div class="blocks">
+        {#each $liturgia.leituras as leitura}
+            <div>{leitura.nome}</div>
+            <div>{leitura.referencia}</div>
+            {#if leitura.descricao}
+                <div>{leitura.descricao}</div>
+            {/if}
+        {/each}
+    </div>
 </main>
--- src/routes/editor/thumbnail/+page.svelte
+++ src/routes/editor/thumbnail/+page.svelte
@@ -1,12 +1,17 @@
-<!-- <script lang="ts">
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
-</script> -->
+<script lang="ts">
+	import { websocketStore, fallback } from '$lib/websocketStore';
+
+	const liturgia = websocketStore('liturgia', fallback['liturgia']);
+</script>
+
 
 <main>
     <h1>thumbnail</h1>
     <canvas>
         
     </canvas>
+
+    <div class="details">
+        {$liturgia.nome} | {new Date().toDateString()}
+    </div>
 </main>
