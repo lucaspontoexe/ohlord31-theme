7ebe93839a421a4be95e0e7dcc03a23b681cf803
lucaspontoexe <ltx16@hotmail.com>
2024-08-20 14:59:15 -0300

    gc background colors marcado pra remoção

--- src/lib/rm-GCBackgroundColor.ts
+++ src/lib/rm-GCBackgroundColor.ts
@@ -14,11 +14,12 @@ export const gradients: Record<string,string> = {
 };
 
 // prettier-ignore
-export const colors: Record<string,string> = {
+const lowerThirdColors: Record<string,string> = {
 	verde:      'rgb(47, 68,  0)',
 	roxo:       'rgb(49, 47, 89)',
 	vermelho:   'rgb(97, 43, 43)',
-	branco:     'rgb(97, 87, 43)'
+	branco:     'rgb(97, 87, 43)',
+	rosa: 		'rgb(155 105 135)',
 }
 
 export const cor = readable('rgb(97, 87, 43)', function start(set) {
