00b2fdda0736213676772d21746e5e1bd498b521
lucaspontoexe <ltx16@hotmail.com>
2024-08-20 15:22:44 -0300

    cores do GC ficam num arquivo em /lib

---
+++ src/lib/LowerThirdColors.ts
@@ -0,0 +1,8 @@
+// prettier-ignore
+export const lowerThirdColors: Record<string,string> = {
+	verde:      'rgb(47, 68,  0)',
+	roxo:       'rgb(49, 47, 89)',
+	vermelho:   'rgb(97, 43, 43)',
+	branco:     'rgb(97, 87, 43)',
+	rosa: 		'rgb(155 105 135)',
+};
--- src/routes/display/gc/+layout.svelte
+++ src/routes/display/gc/+layout.svelte
@@ -2,18 +2,10 @@
 	import { onMount } from 'svelte';
 	import { websocketStore } from '$lib/websocketStore2';
 	import { fallback } from '$lib/fallback';
+	import { lowerThirdColors } from "$lib/LowerThirdColors";
 
 	const store = websocketStore('liturgia', fallback.liturgia);
-
-	// prettier-ignore
-	const lowerThirdColors: Record<string,string> = {
-	verde:      'rgb(47, 68,  0)',
-	roxo:       'rgb(49, 47, 89)',
-	vermelho:   'rgb(97, 43, 43)',
-	branco:     'rgb(97, 87, 43)',
-	rosa: 		'rgb(155 105 135)',
-}
-
+	
 	onMount(() => {
 		const unsubscribe = store.subscribe(({ cor }) =>
 			globalThis.document?.documentElement.style.setProperty(
