ad4a03b361c61ed51cb923f58a2a5f9335c43515
lucaspontoexe <ltx16@hotmail.com>
2024-12-15 13:09:34 -0300

    fix: cor rosa

--- src/lib/LowerThirdColors.ts
+++ src/lib/LowerThirdColors.ts
@@ -4,5 +4,5 @@ export const lowerThirdColors: Record<string,string> = {
 	roxo:       'rgb(49, 47, 89)',
 	vermelho:   'rgb(97, 43, 43)',
 	branco:     'rgb(97, 87, 43)',
-	rosa: 		'rgb(155 105 135)',
+	rosa: 		'rgb(155, 105, 135)',
 };
