2f08c0ac61bc62c5b3df3e4d6994d071d3b214a9
lucaspontoexe <ltx16@hotmail.com>
2024-12-15 13:07:31 -0300

    fix: cor rosa

--- src/routes/gc/+layout.svelte
+++ src/routes/gc/+layout.svelte
@@ -10,7 +10,8 @@
 	verde:      'rgb(47, 68,  0)',
 	roxo:       'rgb(49, 47, 89)',
 	vermelho:   'rgb(97, 43, 43)',
-	branco:     'rgb(97, 87, 43)'
+	branco:     'rgb(97, 87, 43)',
+	rosa: 		'rgb(155, 105, 135)',
 }
 
 	onMount(() => {
